31344fdbe9244ac2e51b939638951fad96051566
Juandario92 <juandariorojas1992@gmail.com>
2023-05-26 15:27:37 -0300

    Implementando React Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Routes, Route } from 'react-router-dom';
+import { Actors, Movies, NavBar, Profile, MovieInformation } from '.';
 
 function App() {
   return (
     <div>
       <CssBaseline />
-      <h1>Hello World - Filmpire</h1>
+      <NavBar />
+      <main>
+        <Routes>
+          <Route exact path="/movie/:id" element={<MovieInformation />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   );
 }
